c61618003be5cee8d3c6ec0f048c90d19d290fcf
Rola <rola2002el@gmail.com>
2025-10-27 18:11:23 +0300

    last

--- server/app.js
+++ server/app.js
@@ -56,10 +56,15 @@ app.use("/api/order", Order);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("*", (req, res) => {
+  app.get("/*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client/dist", "index.html"));
   });
 }
+
+
+app.get('/',(req,res)=>{
+  return res.send('Back run')
+})
 
 const PORT = process.env.PORT;
 
